34362154961ceafde3e5cc868711b45660d35dc4
Cuong. Duong Manh <cuongpigerr@gmail.com>
2024-03-21 11:08:51 +0700

    [feat] complete the client for the volume modifier

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -3,6 +3,7 @@ package client
 import (
 	"context"
 	"fmt"
+	"k8s.io/klog/v2"
 	"time"
 
 	"github.com/kubernetes-csi/csi-lib-utils/connection"
@@ -10,7 +11,7 @@ import (
 	"github.com/kubernetes-csi/csi-lib-utils/rpc"
 	"google.golang.org/grpc"
 
-	modifyrpc "github.com/vngcloud/vngcloud-csi-volume-modifier/pkg/rpc"
+	lmodifyrpc "github.com/vngcloud/vngcloud-csi-volume-modifier/pkg/rpc"
 )
 
 // _____________________________________________________________________________________________________________________PUBLIC METHODS
@@ -41,9 +42,27 @@ func (s *client) GetDriverName(pctx context.Context) (string, error) {
 	return rpc.GetDriverName(pctx, s.conn)
 }
 
-func (s *client) SupportsVolumeModification(ctx context.Context) error {
-	cc := modifyrpc.NewModifyClient(s.conn)
-	req := &modifyrpc.GetCSIDriverModificationCapabilityRequest{}
-	_, err := cc.GetCSIDriverModificationCapability(ctx, req)
+func (s *client) SupportsVolumeModification(pctx context.Context) error {
+	cc := lmodifyrpc.NewModifyClient(s.conn)
+	req := &lmodifyrpc.GetCSIDriverModificationCapabilityRequest{}
+	_, err := cc.GetCSIDriverModificationCapability(pctx, req)
 	return err
+}
+
+func (s *client) Modify(pctx context.Context, pvolumeID string, pparams, preqContext map[string]string) error {
+	cc := lmodifyrpc.NewModifyClient(s.conn)
+	req := &lmodifyrpc.ModifyVolumePropertiesRequest{
+		Name:       pvolumeID,
+		Parameters: pparams,
+		Context:    preqContext,
+	}
+	_, err := cc.ModifyVolumeProperties(pctx, req)
+	if err == nil {
+		klog.V(4).InfoS("Volume modification completed", "volumeID", pvolumeID)
+	}
+	return err
+}
+
+func (s *client) CloseConnection() {
+	_ = s.conn.Close()
 }
